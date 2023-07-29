405eade88a64dd5b462590c9d1d8ad13ed3bbd66
moritak40123 <moritak40123@gmail.com>
2023-07-29 17:22:29 +0900

    candidateコントローラー作成

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   resources :users, only: [:edit, :update]
   resources :rooms do
     resources :schedules, only: [:index, :new, :create, :edit, :update, :destroy]
+    resources :candidates, only: [:index, :new, :create, :edit, :update, :destroy]
   end
 end
